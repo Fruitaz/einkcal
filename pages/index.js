c0f0a93fd0a31289d10651dd896c862e4b0b18a5
Fruitaz <thejustinmcardle@outlook.com>
2024-10-22 21:33:10 +1030

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Screenshot Service</h1>
-      <p>Go to /api/takeScreenshot to trigger the screenshot API.</p>
-    </div>
-  );
-}
