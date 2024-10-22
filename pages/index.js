60abe3d9ac385af166546ace049b919b5c986311
Fruitaz <thejustinmcardle@outlook.com>
2024-10-22 21:51:56 +1030

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Calendar Screenshot App</h1>
+      <p>This is the homepage of your Calendar Screenshot App.</p>
+    </div>
+  );
+}
