fcf0da1c901e491df4ec6712c80fd009be0dd687
Fruitaz <thejustinmcardle@outlook.com>
2024-10-22 21:07:04 +1030

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Screenshot Service</h1>
+      <p>Go to /api/takeScreenshot to trigger the screenshot API.</p>
+    </div>
+  );
+}
