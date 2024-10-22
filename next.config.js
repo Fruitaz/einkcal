a147388f3e171e499a8edc68d1f1b05cdfc322db
Fruitaz <thejustinmcardle@outlook.com>
2024-10-22 18:29:41 +1030

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+
+    return config;
+  },
+};
