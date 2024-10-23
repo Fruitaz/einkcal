62b7378b19531f75bba41a8976dec14751b757c1
Fruitaz <thejustinmcardle@outlook.com>
2024-10-23 16:07:02 +1030

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Calendar Screenshot App</h1>
-      <p>This is the homepage of your Calendar Screenshot App.</p>
-    </div>
-  );
-}
